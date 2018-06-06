7f67abd05870543d557e37f4138353baf12674f7
knownastron <knownastron@gmail.com>
2018-06-06 15:53:21 -0600

    implement testing for value()

--- assignments/assignment3/a3q3_testing.py
+++ assignments/assignment3/a3q3_testing.py
@@ -39,7 +39,7 @@ for card in expected:
 
 
 #####################################################################
-# test Statistics.create()
+# test Card.deal()
 #Integration testing
 
 
@@ -104,5 +104,34 @@ for t in test_deal:
                   'cards remaining in deck but found', len(thing),
                   'cards remaining in deck', '--', t['reason'])
 
+#####################################################################
+# test Card.value()
+#Integration testing
+
+
+test_value = [
+    {'inputs' :['AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H',
+                'JH', 'QH', 'KH',
+                'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D',
+                'JD', 'QD', 'KD',
+                'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S',
+                'JS', 'QS', 'KS',
+                'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C',
+                'JC', 'QC', 'KC',],
+     'outputs': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], #[hands dealt, cards per hand, cards remaining in deck]
+     'reason' : 'comprehensive testing, all cards in deck are tested'},
+]
+
+for t in test_value:
+    args_in = t['inputs']
+    expected = t['outputs']
+
+    #create the Card data structure
+    thing = Card.create()
+
+    for index, card in enumerate(thing):
+        if Card.value(card) != expected[index % 13]:
+            print('Error in Card.value(), expected', expected[index % 13],
+            'but got', Card.value(card))
 
 print('*** Test script completed ***')
