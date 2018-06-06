1bb19b0aa1ce9e5707b986e7c2154180ae87f8cf
knownastron <knownastron@gmail.com>
2018-06-06 11:02:49 -0600

    implement test for create()

--- assignments/assignment3/a3q3_testing.py
+++ assignments/assignment3/a3q3_testing.py
@@ -5,3 +5,29 @@
 #Lab: L03
 
 #Assignment 3 Question 3 testing script
+
+import a3q3 as Card
+
+#####################################################################
+# test Statistics.create()
+#Unit testing
+
+test_create = [
+    {'inputs' : [],
+     'outputs':['AH', 'KC' , '4S', 'JC', '8D', 'yas'], #[first, last, random, random, random]
+     'reason' : 'Check first & last value and 3 arbitrarily chosen values'},
+]
+
+for t in test_create:
+    args_in = t['inputs']
+    expected = t['outputs']
+
+#create the Card data structure
+thing = Card.create()
+
+for output in expected:
+    if output not in thing:
+        print('Error in create(): expected card', expected[2],
+              'did not appear in deck', '--', t['reason'])
+
+print('*** Test script completed ***')
