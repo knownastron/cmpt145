ed18800ad34afa555fb9c425544a297cade8fc87
knownastron <knownastron@gmail.com>
2018-06-13 11:32:15 -0600

    implement print_balance() into main REPL - complete

--- assignments/assignment4/a4q3.py
+++ assignments/assignment4/a4q3.py
@@ -7,15 +7,15 @@
 
 import a4q2 as Check
 
-console_input = ''
-
 print('Enter an expression to check if the parentheses are balanced.',
         'Enter \'quit\' to end program:')
+
 while True:
     console_input = input('>')
     if console_input == 'quit':
         break
     else:
         result = Check.check_balance(console_input)
-        print(result)
+        Check.print_balance(result)
+
 print("Goodbye!")
