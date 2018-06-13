aeffd39d836b1f8e240f28c32fc41123640d8e7f
knownastron <knownastron@gmail.com>
2018-06-12 21:50:13 -0600

    complete program

--- assignments/assignment4/a4q2.py
+++ assignments/assignment4/a4q2.py
@@ -3,3 +3,36 @@
 #Student Number: 11101081
 #Course: CMPT 145-01
 #Lab: L03
+
+import sys
+import TStack as Stack
+
+
+
+expression = sys.argv[1]
+
+para_stack = Stack.create()
+
+for char in expression:
+    #checks for opening parentheses
+    if char == '(' or char == '[' or char == '{':
+        Stack.push(para_stack, char)
+
+    #checks for closing parentheses
+    elif char == ')':
+        if Stack.is_empty(para_stack):
+            continue
+        elif Stack.peek(para_stack) == '(' :
+            Stack.pop(para_stack)
+    elif char == ']':
+        if Stack.is_empty(para_stack):
+            continue
+        elif Stack.peek(para_stack) == '[' :
+            Stack.pop(para_stack)
+    elif char == '}':
+        if Stack.is_empty(para_stack):
+            continue
+        elif Stack.peek(para_stack) == '{' :
+            Stack.pop(para_stack)
+
+print(Stack.is_empty(para_stack))
