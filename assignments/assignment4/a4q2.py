c3c7c988cf8386fa32702eacbb0c245d252ff255
knownastron <knownastron@gmail.com>
2018-06-12 21:57:26 -0600

    refactored main program as a function for testing

--- assignments/assignment4/a4q2.py
+++ assignments/assignment4/a4q2.py
@@ -13,26 +13,31 @@ expression = sys.argv[1]
 
 para_stack = Stack.create()
 
-for char in expression:
-    #checks for opening parentheses
-    if char == '(' or char == '[' or char == '{':
-        Stack.push(para_stack, char)
+def check_parentheses(para_stack):
+    for char in expression:
+        #checks for opening parentheses
+        if char == '(' or char == '[' or char == '{':
+            Stack.push(para_stack, char)
 
-    #checks for closing parentheses
-    elif char == ')':
-        if Stack.is_empty(para_stack):
-            continue
-        elif Stack.peek(para_stack) == '(' :
-            Stack.pop(para_stack)
-    elif char == ']':
-        if Stack.is_empty(para_stack):
-            continue
-        elif Stack.peek(para_stack) == '[' :
-            Stack.pop(para_stack)
-    elif char == '}':
-        if Stack.is_empty(para_stack):
-            continue
-        elif Stack.peek(para_stack) == '{' :
-            Stack.pop(para_stack)
+        #checks for closing parentheses
+        elif char == ')':
+            if Stack.is_empty(para_stack):
+                return False
+            elif Stack.peek(para_stack) == '(' :
+                Stack.pop(para_stack)
+        elif char == ']':
+            if Stack.is_empty(para_stack):
+                return False
+            elif Stack.peek(para_stack) == '[' :
+                Stack.pop(para_stack)
+        elif char == '}':
+            if Stack.is_empty(para_stack):
+                return False
+            elif Stack.peek(para_stack) == '{' :
+                Stack.pop(para_stack)
+    return (Stack.is_empty(para_stack))
 
-print(Stack.is_empty(para_stack))
+if len(sys.argv) == 2:
+    expression = sys.argv[1]
+    para_stack = Stack.create()
+    print(check_parentheses(para_stack))
