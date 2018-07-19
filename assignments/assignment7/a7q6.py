7eccdc232cf9a14e036547530c12a1b9ef091424
knownastron <knownastron@gmail.com>
2018-07-18 20:04:19 -0600

    added docstring for average(), reverse_chain() and copy()

---
+++ assignments/assignment7/a7q6.py
@@ -0,0 +1,40 @@
+#Name: Jason Tran
+#NSID: jat687
+#Student Number: 11101081
+#Course: CMPT 145-01
+#Lab: L03
+
+def average(node_chain):
+    """
+    Purpose:
+        returns the average of all the data in the node chain
+    Pre-conditions:
+        :param node_chain: a node chain, possibly empty, only contains numbers as data
+    Return:
+        :return: The average of all the data in the node chain
+    """
+    pass
+
+def reverse_chain(node_chain):
+    """
+    Purpose:
+        Completely reverses the order of the given node_chain.
+    Pre-conditions:
+        :param node_chain: a node chain, possibly empty
+    Post-conditions:
+        The front of the node_chain is altered to be the back, with all nodes now pointing next the opposite direction.
+    Return:
+        :return: The resulting node chain that has had its order reversed
+    """
+    pass
+
+def copy(node-chain):
+    """
+    Purpose:
+        Create a new node-chain
+    Pre-conditions:
+        :param node_chain: a node chain, possibly empty
+    return:
+        :return: a seperate distinct chain that has the same values as node-chain
+    """
+    pass
