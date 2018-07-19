6ba8d21250d700bafd46dbec0fb54d0fd934cbfb
knownastron <knownastron@gmail.com>
2018-07-18 21:21:13 -0600

    implement average()

--- assignments/assignment7/a7q6.py
+++ assignments/assignment7/a7q6.py
@@ -4,7 +4,9 @@
 #Course: CMPT 145-01
 #Lab: L03
 
-def average(node_chain):
+import node as node
+
+def average(node_chain, sum=0, count=0):
     """
     Purpose:
         returns the average of all the data in the node chain
@@ -13,9 +15,16 @@ def average(node_chain):
     Return:
         :return: The average of all the data in the node chain
     """
-    pass
+    if node_chain is None:
+        if count == 0:
+            return 0
+        else:
+            return sum/count
+    else:
+        value = node.get_data(node_chain)
+        return average(node.get_next(node_chain), sum + value, count + 1)
 
-def reverse_chain(node_chain):
+def reverse_chain(node_chain, prev_node=None):
     """
     Purpose:
         Completely reverses the order of the given node_chain.
@@ -26,9 +35,15 @@ def reverse_chain(node_chain):
     Return:
         :return: The resulting node chain that has had its order reversed
     """
-    pass
+    if node_chain is None:
+        return prev_node
+    else:
+        next = node.get_next(node_chain)
+        node.set_next(node_chain, prev_node)
+        return reverse_chain(next, node_chain)
 
-def copy(node-chain):
+
+def copy(node_chain):
     """
     Purpose:
         Create a new node-chain
@@ -38,3 +53,14 @@ def copy(node-chain):
         :return: a seperate distinct chain that has the same values as node-chain
     """
     pass
+
+
+
+
+if __name__ == '__main__':
+    several_node = node.create(0)
+    for i in range(1,5):
+        several_node = node.create(i, several_node)
+
+    print(node.to_string(several_node))
+    print(node.to_string(reverse_chain(several_node)))
